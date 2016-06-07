e537ce925f665e04e35e24a2c14a55ffeec87a4e
rosiv <rosiu@outlook.com>
2016-06-07 22:01:44 +0200

    Add product triggers

---
+++ BackOffice.EventProviders.SqlEventProvider/SqlScripts/Triggers/Products_AfterDelete_Trigger.sql
@@ -0,0 +1,29 @@
+﻿DROP TRIGGER Products_AfterDelete_Trigger
+GO
+
+CREATE TRIGGER Products_AfterDelete_Trigger
+    ON [BackOffice].[dbo].[Products]
+ AFTER DELETE
+AS
+BEGIN
+	DECLARE @h UNIQUEIDENTIFIER ;
+	DECLARE @id varchar(10);
+	DECLARE @name varchar(10);
+	DECLARE @description varchar(15);
+
+	SELECT @id = CONVERT(varchar, i.id) FROM deleted i;
+	SELECT @name = i.name FROM deleted i;
+	SELECT @description = i.description FROM deleted i;
+
+	BEGIN DIALOG CONVERSATION @h 
+	   FROM SERVICE ProductChanged_Service_Sender 
+	   TO SERVICE 'ProductChanged_Service_Receiver' 
+	   ON CONTRACT ProductChanged_Contract 
+	   WITH ENCRYPTION=OFF 
+
+	;SEND ON CONVERSATION @h  
+	MESSAGE TYPE ProductChanged_Msg 
+	('<Message type="Product_Deleted"><id>'+@id+'</id><name>'+@name+'</name><description>'+@description+'</description></Message>') 
+
+END
+GO
---
+++ BackOffice.EventProviders.SqlEventProvider/SqlScripts/Triggers/Products_AfterUpdate_Trigger.sql
@@ -0,0 +1,26 @@
+﻿CREATE TRIGGER Products_AfterUpdate_Trigger
+    ON [BackOffice].[dbo].[Products]
+ AFTER UPDATE
+AS
+BEGIN
+	DECLARE @h UNIQUEIDENTIFIER ;
+	DECLARE @id varchar(10);
+	DECLARE @name varchar(10);
+	DECLARE @description varchar(15);
+
+	SELECT @id = CONVERT(varchar, i.id) FROM inserted i;
+	SELECT @name = i.name FROM inserted i;
+	SELECT @description = i.description FROM inserted i;
+
+	BEGIN DIALOG CONVERSATION @h 
+	   FROM SERVICE ProductChanged_Service_Sender 
+	   TO SERVICE 'ProductChanged_Service_Receiver' 
+	   ON CONTRACT ProductChanged_Contract 
+	   WITH ENCRYPTION=OFF 
+
+	;SEND ON CONVERSATION @h  
+	MESSAGE TYPE ProductChanged_Msg 
+	('<Message type="Product_Updated"><id>'+@id+'</id><name>'+@name+'</name><description>'+@description+'</description></Message>') 
+
+END
+GO
