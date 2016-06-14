5b409443a331f76afb1ab6092e34ce19c12917f8
rosiv <rosiu@outlook.com>
2016-06-15 01:11:44 +0200

    Generate simple xml report file

--- BackOffice.Worker/BProductSimpleExcelReportWorker.cs
+++ BackOffice.Worker/BProductSimpleExcelReportWorker.cs
@@ -1,4 +1,5 @@
-﻿using BackOffice.Jobs.Interfaces;
+﻿using BackOffice.Common;
+using BackOffice.Jobs.Interfaces;
 using BackOffice.Jobs.Reports;
 using NPOI.POIFS.FileSystem;
 using NPOI.SS.UserModel;
@@ -10,7 +11,7 @@ namespace BackOffice.Worker
     public class BProductSimpleExcelReportWorker : IJobWorker
     {
         private readonly SimpleExcelReport report;
-        private const string ReportFile = "Reports\\B_Products_Report.xls";
+        private const string ReportFile = "Reports\\B_Products_Report.xlsx";
 
         public BProductSimpleExcelReportWorker(SimpleExcelReport report)
         {
@@ -19,58 +20,57 @@ namespace BackOffice.Worker
 
         public void Start()
         {
-            if (!File.Exists(ReportFile))
+            var fi = new FileInfo(ReportFile);
+
+            if (!fi.Exists)
             {
-                if (!Directory.Exists("Reports"))
+                if (!fi.Directory.Exists)
                 {
-                    Directory.CreateDirectory("Reports");
+                    fi.Directory.Create();
                 }
             }
-            using (FileStream fs = new FileStream("Reports\\B_Products_Report.xlsx", System.IO.FileMode.Open))
+
+            IWorkbook workbook;
+            using (FileStream fs = new FileStream(fi.FullName, System.IO.FileMode.OpenOrCreate, FileAccess.Read))
             {
-                var workbook = WorkbookFactory.Create(fs);
-                var worksheet = workbook.GetSheetAt(0);
-
-
-                for (int lineNumber = 1; true; lineNumber++)
-                {
-                    var row = worksheet.GetRow(lineNumber);
-
-                    if (row != null )
-                    {
-                        continue;
-                    }
-
-                    row = worksheet.CreateRow(0);
-                    row.CreateCell(0);
-                    row.CreateCell(1);
-                    row.CreateCell(2);
-                    row.CreateCell(3);
-
-                    var whatHappenedCell = row.GetCell(0);
-                    var productIdCell = row.GetCell(1);
-                    var productNameCell = row.GetCell(2);
-                    var productDescriptionCell = row.GetCell(3);
-
-                    whatHappenedCell.SetCellValue(this.report.Data.Action);
-                    productIdCell.SetCellValue(this.report.Data.Product.Id);
-                    productNameCell.SetCellValue(this.report.Data.Product.Name);
-                    productDescriptionCell.SetCellValue(this.report.Data.Product.Description);
-
-                    workbook.Write(fs);
-                    workbook.Close();
-                }
+                workbook = WorkbookFactory.Create(fs);
             }
-            var txt = string.Format("|{0}|{1}|{2}|{3}|\r\n",
-                report.Data.Action.PadRight(15),
-                report.Data.Product.Id.ToString().PadRight(4),
-                report.Data.Product.Name.PadRight(15),
-                report.Data.Product.Description.PadRight(20));
+            var worksheet = workbook.GetSheetAt(0);
 
-            //remove this
-            Thread.Sleep(10000);
+            for (int lineNumber = 1; true; lineNumber++)
+            {
+                var row = worksheet.GetRow(lineNumber);
 
-            File.AppendAllLines(ReportFile, new[] { txt });
+                if (row != null)
+                {
+                    continue;
+                }
+
+                row = worksheet.CreateRow(lineNumber);
+                row.CreateCell(0);
+                row.CreateCell(1);
+                row.CreateCell(2);
+                row.CreateCell(3);
+
+                var whatHappenedCell = row.GetCell(0);
+                var productIdCell = row.GetCell(1);
+                var productNameCell = row.GetCell(2);
+                var productDescriptionCell = row.GetCell(3);
+
+                whatHappenedCell.SetCellValue(this.report.Data.Action);
+                productIdCell.SetCellValue(this.report.Data.Product.Id);
+                productNameCell.SetCellValue(this.report.Data.Product.Name);
+                productDescriptionCell.SetCellValue(this.report.Data.Product.Description);
+
+
+                //remove this
+                Thread.Sleep(10000);
+                Logging.Log().Debug("Writing to file {file}", ReportFile);
+                using (var fss = new FileStream(ReportFile, FileMode.Truncate, FileAccess.Write))
+                    workbook.Write(fss);
+
+                break;
+            }
         }
     }
 }
